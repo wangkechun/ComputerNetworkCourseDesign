b93786879f94032916439ad647c7007be3bbcdd2
wangkechun <hi@hi-hi.cn>
2014-12-30 17:04:16 +0800

    fabfile

--- fabfile.py
+++ fabfile.py
@@ -3,3 +3,14 @@ from fabric.api import *
 
 def rp():
 	local("go build pingScan.go&&sudo  ./pingScan ")
+
+
+def tr():
+	local("go build tracert.go&&sudo  ./tracert ")
+
+
+def echo():
+	local("go build echo.go&&sudo  ./echo ")
+
+def tr2():
+	local("go build traceroute.go&&sudo  ./traceroute baidu.com ")
